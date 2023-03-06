b1e1a17b62404b71c5e447cf2c0c02afb7dad6d5
jef901 <39604356+jef901@users.noreply.github.com>
2023-03-06 03:24:02 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prueba para sitio React y Azure
         </p>
         <a
           className="App-link"
