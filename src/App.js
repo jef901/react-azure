10ba61c70cba7b52b2d90926adeb05a1b19b9d7f
jef901 <39604356+jef901@users.noreply.github.com>
2023-03-06 04:28:32 -0300

    creando una modificacion

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Prueba para sitio React y Azure
+          Realizando una nueva modificacion
         </p>
         <a
           className="App-link"
